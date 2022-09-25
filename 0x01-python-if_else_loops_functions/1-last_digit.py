7e473d4b4d3e332054ab61954cb941e751d10a84
ochiengsulwe <kodongochieng@gail.com>
2022-09-25 06:57:59 -0700

    checking the last digit of a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,6 @@ if ld > 5:
 elif ld == 0:
     print(f"Last digit of {number} is {ld} and is 0")
 
-elif ld < 6 and not 0:
-    print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
+else:
+    if ld < 6 and not 0:
+        print(f"Last digit of {number} is {ld} and is less than 6 and not 0")
