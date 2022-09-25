ab9f6ca4e8b9a22a9cfde3ad0fc6d8438eb13998
ochiengsulwe <kodongochieng@gail.com>
2022-09-25 06:36:30 -0700

    checking the last digit of a random number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,6 @@ if ld > 5:
 elif ld == 0:
     print(f"Last didgit of {number} is {ld} and is 0")
 
-elif (ld < 6 && != 0)
+else
     print(f"Last didgit of {number} is {ld} and is less than 6 and not 0")
 
