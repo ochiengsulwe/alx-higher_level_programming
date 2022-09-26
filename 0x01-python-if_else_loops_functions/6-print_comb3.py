e8e47d6b0fa8dabb5690fb0a9bcc3ca211150e3e
ochiengsulwe <kodongochieng@gail.com>
2022-09-26 12:29:57 -0700

    printing number combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    while not((str(i)[::-1]) == i):
-        end=", " if i < 89 else end=""
+    while ((str(i)[::-1]) != str(i)):
+        end = ", " if i < 89 else end = ""
     print("{:02d}".format(i), end=end)
