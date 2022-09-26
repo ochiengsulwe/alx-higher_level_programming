5d381e4f4a56a4b75867f3f1dc73e4289fc84ab6
ochiengsulwe <kodongochieng@gail.com>
2022-09-26 08:41:36 -0700

    printing range(100) seperated by comas

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for x in range(100):
-    end=", " if x < 99 else " "
-    print("{}".format(x), end=", ")
+    end = ", " if x < 99 else " "
+    print("{}".format(x), end = end)
