6476d49251a6b34f6b555df6ed7e5cdb9ca0e746
ochiengsulwe <kodongochieng@gail.com>
2022-09-26 09:44:52 -0700

    printing combination of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    while !((str(i)[::-1]) == x)
+    while not((str(i)[::-1]) ==i)
     end = ", " if i < 89 else end = ""
-    print("{:02d}".format(x), end=end)
+    print("{:02d}".format(i), end=end)
