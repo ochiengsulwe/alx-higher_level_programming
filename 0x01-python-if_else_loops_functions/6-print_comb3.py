56162d5b973f2a0b41d4c96eab0db82639badfdf
ochiengsulwe <kodongochieng@gail.com>
2022-09-26 22:51:59 -0700

    printing int combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-while j is not i:
-    for i in range(100):
-        for j in range(i+1, 100):
-            end = ", " if i == 89 else " "
+for i in range(100):
+    for j in range(i+1, 100):
+        end = ", " if i == 89 else " "
     print("{:02d}{:02d}".format(i, j), end=end)
