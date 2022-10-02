603624a2171b5a0b32c29d1844465f406df35842
ochiengsulwe <kodongochieng@gail.com>
2022-10-02 07:40:59 -0700

    printing in range 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-for x in range(100):
-    end = ", " if x < 99 else " "
-    print("{:02d}".format(x), end=end)
+# for x in range(100):
+#  end = ", " if x < 99 else " "
+#   print("{:02d}".format(x), end=end)
+for x in range(0, 99):
+    print("{0:02d}".format(i), end=", ")
+print("{:f}".format(99))
