89699fa40eff0fdabd7d023ba590177b58feb7dc
ochiengsulwe <kodongochieng@gail.com>
2022-10-16 04:21:29 -0700

    function doing some math

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# addition function
+def add(a, b):
+    return (a + b)
+
+# subtruction function
+def sub(a, b):
+    return (a - b)
+
+# multiplication function
+def mul(a, b):
+    return (a * b)
+
+# division function
+def div(a, b):
+    return(a \ b)
