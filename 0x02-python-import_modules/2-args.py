c979d01e1d3461e1450a098aabbccbf0c0c3708c
ochiengsulwe <kodongochieng@gail.com>
2022-10-17 05:00:43 -0700

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 # a function to list number of arguments in a function
 if __name__ == "__main__":
-    length = len(sys.argv)
+    length = len(argv)
+    argv = []
+    index = argv.index(argv[] + 1)
 
-    if length == 0:
-	print("{:d} arguments." .format(length - 1))
-    elif lenght == 1:
-        print("{:d} argument:".format(length - 1))
-    else:
-        print("{:d} arguments:".format(length - 1))
-    for argv in range(1, length):
-        print("{:d} : {:s}".format(argv, sys.argv[argv]))
+    if length == 1:
+    print("{:d} argument: ".format(length))
+    print("{:s}".format(argv))
+    elif length is not 1:
+        if length == 0:
+	print("{:d} arguments. ".format(length))
+	else:
+	for k in range(length):
+	print("{:d} arguments: ".format(length))
+        print("{:d} : {:s} ".format(index, argv))
