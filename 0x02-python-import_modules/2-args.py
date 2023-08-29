54c051f3987ae9074d221c6c414977791c3d3218
ochiengsulwe <kodongochieng@gmail.com>
2023-08-29 05:58:48 -0700

    tree prunning

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     length = len(argv)
     
     for k in range(length):
-    print("{:d} argument: ".format(length))
-    print("{:s}".format(argv[k])
+        print("{:d} argument: ".format(length))
+        print("{:s}".format(argv[k])
     else:
     if length == 0:
     print("{:d} arguments.".format(length))
