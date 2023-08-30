a59b907be846bfe9645607ba3f20bd7023b8ae1a
ochiengsulwe <kodongochieng@gmail.com>
2023-08-30 07:59:09 -0700

    only integers on my screen

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for item in my_list:
+        try:
+            print("{:d}".format(item), end=' ')
+            count += 1
+            if count == x:
+                break
+        except (ValueError, TypeError):
+            pass  # Skip non-integer items
+
+    print()
+    return count
