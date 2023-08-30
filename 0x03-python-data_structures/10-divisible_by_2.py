5f89b95948c4f86b056eb65e98b2820b94688439
ochiengsulwe <kodongochieng@gmail.com>
2023-08-30 05:19:41 -0700

    Am I even

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     result_list = []
-    for items in my_list:
-        result_list.append(item % 2 == 0)
+    for item in my_list:
+        result = item % 2 == 0
+        if result:
+            result_list.append(True)
+            print(f"{item} is divisible by 2")
+        else:
+            result_list.append(False)
+            print(f"{item} is not divisible by 2")
     return result_list
