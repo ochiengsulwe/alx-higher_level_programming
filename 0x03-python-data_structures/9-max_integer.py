c08fd7a84f00ccd6c7285f4cb12b059e8977f19d
ochiengsulwe <kodongochieng@gmail.com>
2023-08-30 04:57:48 -0700

    find me the biggest in here

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    val_max = my_list[0]
+    for item in my_list:
+        if val_max > item:
+            val_max = item
+    return val_max
