652a01d202efc0b5bb7c83e609745d230cebf3ef
ochiengsulwe <kodongochieng@gmail.com>
2023-08-30 08:51:49 -0700

    cleanup

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if count == x:
                 break
         except (ValueError, TypeError):
-            pass  # Skip non-integer items
+            pass
 
     print()
     return count
