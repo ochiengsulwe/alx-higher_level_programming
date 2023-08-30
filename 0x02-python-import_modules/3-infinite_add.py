262e441f04ac76c5e8650873a690efe4a55010e8
ochiengsulwe <kodongochieng@gmail.com>
2023-08-30 03:26:50 -0700

    push me 8 more spaces in

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,12 +2,17 @@
 
 import sys
 
-length = len(sys.argv)
-summ = 0
-if __name__ == "__main__":
-    if length == 1:
+def calculate_sum(arguments):
+    length = len(arguments)
+    summ = 0
+    if length == 0:
         summ = 0
     else:
-        for i in range(1, length):
-            summ += int(sys.argv[i])
-        print("{:d}".format(summ))
+        for i in range(length):
+            summ += int(arguments[i])
+    return summ
+
+if __name__ == "__main__":
+    summ = calculate_sum(sys.argv[1:])
+    print("{:d}".format(summ))
+
