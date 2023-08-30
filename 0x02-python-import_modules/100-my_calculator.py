2b58b448456021d6e08174461b3d297ee6439cfd
ochiengsulwe <kodongochieng@gmail.com>
2023-08-30 03:47:22 -0700

    make 'em neat

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 
 from calculator_1 import add, sub, mul, div
-from sys import argv
+import sys
+
+def validate_input(args):
+    if len(args) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+operator_mapping = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div
+}
 
 if __name__ == "__main__":
-    length = len(argv)
-    if length != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-         exit(1)
-    if length == 4:
-        operator = argv[2]
-        a = int(argv[1])
-        b = int(argv[3])
-        if operator == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif operator == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif operator == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif operator == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    validate_input(sys.argv)
 
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator in operator_mapping:
+        result = operator_mapping[operator](a, b)
+        print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
