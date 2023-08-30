f09dd5fde9a12652c274a2ba9f926a71263ead95
ochiengsulwe <kodongochieng@gmail.com>
2023-08-30 08:46:19 -0700

    code refactor

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,6 @@ def square_matrix_simple(matrix=[]):
     lenM = len(matrix)
     new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(lenM)]
     for i in range(len(matrix)):
-        new_matrix[i][j] = matrix[i][j]*matrix[i][j]
+        for j in range(len(matrix[0])):
+            new_matrix[i][j] = matrix[i][j] * matrix[i][j]
     return new_matrix
