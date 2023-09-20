1188af72ef9d0d6019c954811972e3bf5e1e84f9
ochiengsulwe <kodongochieng@gmail.com>
2023-09-20 02:36:44 -0700

    instantiating

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 """a class Square that defines a square by: (based on 1-square.py)"""
 
 
 class Square:
+
     """A square definition"""
 
     def __init__(self, size=0):
+
         """instantiating my class with size of default value 0.
 
         Args:
