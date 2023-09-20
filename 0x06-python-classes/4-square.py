5d3b549671152267ac23222e584d9162475c9eb0
ochiengsulwe <kodongochieng@gmail.com>
2023-09-20 08:47:11 -0700

    setting and getting

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,25 +3,28 @@
 
 
 class Square:
+
     """a class definning a shape of equal foursides.
 
     This class illustrates a shape of same size.
     """
 
     def __init__(self, size=0):
-    """instantiating the classs with optional field size.
 
-    Args:
-        size(:obj: `int`, optional): the shape dimension
-    """
-    self.__size = size
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
+        """instantiating the classs with optional field size.
+
+        Args:
+            size(:obj: `int`, optional): the shape dimension
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
     @property
     def size(self):
+
         """int: retrives the value of private field size"""
 
         return (self.__size)
