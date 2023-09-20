ffcf0a61e02b240e48b13f943b34b5164c4a0d78
ochiengsulwe <kodongochieng@gmail.com>
2023-09-20 02:17:55 -0700

    instantiating

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a sized square"""
+
+class Square:
+
+    """Square with intialisez size"""
+
+    def __init__(self, size):
+
+        """Instantiating our square"""
+
+        self.__size = size
