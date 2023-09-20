23405cf31c03c749d0502587643e017790c14d74
ochiengsulwe <kodongochieng@gmail.com>
2023-09-20 06:28:06 -0700

    instantiating with size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-"""a class Square that defines a square by: (based on 1-square.py)"""
+"""a class Square that defines a square by: (based on 1-square.py)."""
 
 
 class Square:
 
-    """A square definition"""
+    """A square definition. Instantiated with field size"""
 
     def __init__(self, size=0):
 
@@ -17,6 +17,6 @@ class Square:
 
         self.__size = size
         if not isinstance(size, int):
-            raise TypeError(size must be an integer)
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError(size must be >= 0)
+            raise ValueError("size must be >= 0")
