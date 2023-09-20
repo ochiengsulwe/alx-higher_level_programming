5afd3adb043b2719e0afa03a8f038868a2e85063
ochiengsulwe <kodongochieng@gmail.com>
2023-09-20 01:19:28 -0700

    starting my classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """ This is an non-functional sqaure class yet """
+    """ This is an non-functional sqaure class yet.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
     pass
