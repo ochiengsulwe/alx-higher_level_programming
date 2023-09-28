9406ad87ea480b411e3d782ea0b89e446d434045
ochiengsulwe <kodongochieng@gmail.com>
2023-09-28 07:37:01 -0700

    space me less

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for item in my_list:
         try:
-            print("{:d}".format(item), end=' ')
+            print("{:d}".format(item), end='')
             count += 1
             if count == x:
                 break
