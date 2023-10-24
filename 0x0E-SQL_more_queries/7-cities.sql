a8aa0680b13f40f97780be17f210ab98d0443d0c
ochiengsulwe <kodongochieng@gmail.com>
2023-10-24 09:18:29 -0700

    foreign keys

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -10,7 +10,7 @@ USE `hbtn_0d_usa`;
 
 CREATE TABLE IF NOT EXISTS `states` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
-	`name` VARCHAR(256)
+	`name` VARCHAR(256) NOT NULL
 	);
 
 -- create cities  table
