81078062389347b9923020fdb4ae09f3987fd98d
ochiengsulwe <kodongochieng@gmail.com>
2023-10-25 06:35:57 -0700

    tryingnto find the error

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,18 +3,9 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 
 -- specify database to use
-
-USE hbtn_0d_usa;
-
--- creates states table
-
-CREATE TABLE IF NOT EXISTS `states` (
-	`id` INT AUTO_INCREMENT PRIMARY KEY,
-	`name` VARCHAR(256) NOT NULL
-	);
+USE `hbtn_0d_usa`;
 
 -- create cities  table
-
 CREATE TABLE IF NOT EXISTS `cities` (
 	`id` INT AUTO_INCREMENT PRIMARY KEY,
 	`state_id` INT NOT NULL,
