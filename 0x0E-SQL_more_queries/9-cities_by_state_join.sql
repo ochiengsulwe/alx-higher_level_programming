77e17b3a23e9aa6150fcd6533f1c678fb1e84f18
ochiengsulwe <kodongochieng@gmail.com>
2023-11-04 10:51:15 -0700

    find states

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- Script to list all cities with their respective states
+
+-- Use the specified database passed as an argument
+USE $1;
+
+-- List all cities with their respective states
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
