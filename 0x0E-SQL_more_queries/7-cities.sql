3f8258b2fe2fe75d28e70f2ca85f0edf28380647
ochiengsulwe <kodongochieng@gmail.com>
2023-11-04 10:41:13 -0700

    fixing cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,14 +1,14 @@
 --create database
 
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- specify database to use
-USE `hbtn_0d_usa`;
+USE hbtn_0d_usa;
 
 -- create cities  table
-CREATE TABLE IF NOT EXISTS `cities` (
-	`id` INT AUTO_INCREMENT PRIMARY KEY,
-	`state_id` INT NOT NULL,
-	FOREIGN KEY (`state_id`) REFERENCES `states` (`id`),
-	`name` VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states (id)
 	);	
