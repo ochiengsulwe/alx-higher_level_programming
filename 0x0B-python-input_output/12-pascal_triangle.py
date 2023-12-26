11ff00bf4eacc75ad417b2cf497cbdbf911419fa
ochiengsulwe <kodongochieng@gmail.com>
2023-12-26 03:10:23 -0800

    some littele refactor

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -17,14 +17,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
+    line = [[0 for x in range(i + 1)] for i in range(n)]
+    line[0] = [1]
 
     for i in range(1, n):
-        l[i][0] = 1
+        line[i][0] = 1
         for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
+            if j < len(line[i - 1]):
+                line[i][j] = line[i - 1][j - 1] + line[i - 1][j]
             else:
-                l[i][j] = l[i - 1][0]
-    return l
+                line[i][j] = line[i - 1][0]
+    return line
