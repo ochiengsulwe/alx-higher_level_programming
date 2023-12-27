52574812c46382538c3ea4ff832d1414d6241a30
ochiengsulwe <kodongochieng@gmail.com>
2023-12-27 08:40:24 -0800

    constraints: value must be positive, and of type integer

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,7 +62,7 @@ class Rectangle(Base):
     def x(self, value):
         if type(value) != int:
             raise TypeError("x must be an integer")
-         if value < 0:
+        if value < 0:
              raise ValueError("x must be >= 0")
         self.__x = value
 
