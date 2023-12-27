44a0e391f9a8f37be7fa117f3c44f7bf85677aab
ochiengsulwe <kodongochieng@gmail.com>
2023-12-27 00:30:56 -0800

    init

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
-""" The base clas which defines the other classes"""
+""" The base class which defines the other classes in this project
+    
+    The goal of it is to manage the `id` in all the future classes
+    and to prevent duplication of some code and by estension, some bugs.
+"""
 
 class Base(object):
+    """The base class managing the `id` attribute in the project
 
+    Attributes:
+        __nb_objects (int): Private class attribute use to track `id`.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The class is instatited with one optional public attribute `id`.
+
+        Args:
+            id (:obj: `int`, optional): A public attribute to be tracked.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
