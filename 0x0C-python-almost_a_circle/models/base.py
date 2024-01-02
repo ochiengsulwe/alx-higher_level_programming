4caf8e20397dd689794182a5ba62115a5b60ca2a
ochiengsulwe <kodongochieng@gmail.com>
2024-01-02 04:46:29 -0800

    serialising my class attributes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
     The goal of it is to manage the `id` in all the future classes
     and to prevent duplication of some code and by estension, some bugs.
 """
+import json
 
 
 class Base(object):
@@ -18,10 +19,25 @@ class Base(object):
         """The class is instatited with one optional public attribute `id`.
 
         Args:
-            id (:obj: `int`, optional): A public attribute to be tracked.
+            id (:obj:`int`, optional): A public attribute to be tracked.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Describess the JSON string representation of `list_dictionaries`.
+
+        Args:
+            list_dictionaries (:obj:`list` of :obj:`dict`):
+                A list of instance attributes.
+
+        Returns:
+            str: JSON representation of `list_dictionaries`.
+        """
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
