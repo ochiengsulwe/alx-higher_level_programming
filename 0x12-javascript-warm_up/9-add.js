14f2bfb7332aaa157ead39024cf9d5da5cfcc26c
ochiengsulwe <kodongochieng@gmail.com>
2024-01-08 11:12:45 -0800

    adding them up

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (!isNaN(num1) && !isNaN(num2)) {
+  const sum = add(num1, num2);
+  console.log(sum);
+} else {
+  console.log('NaN');
+}
