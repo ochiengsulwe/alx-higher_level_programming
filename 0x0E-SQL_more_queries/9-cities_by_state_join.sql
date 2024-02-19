bb30c9c88831bf3ff9f147e6389df42bc5dae44b
ochiengsulwe <kodongochieng@gmail.com>
2024-02-19 06:07:03 -0800

    Listing all

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,7 +3,7 @@
 -- Use the specified database passed as an argument.
 
 -- List all cities with their respective states
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name AS name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
