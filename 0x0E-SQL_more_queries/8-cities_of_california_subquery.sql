33cb4e055f290d9f090fa4222568b1c17b07e138
ochiengsulwe <kodongochieng@gmail.com>
2024-02-19 05:59:46 -0800

    Ordering cities

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Script to list cities of California without using `JOIN`
 
-SELECT cities.name
+SELECT cities.id, cities.name
 FROM cities, states
 WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
