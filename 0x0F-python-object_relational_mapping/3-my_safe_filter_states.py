bf6665e8f2e4bbd5e8e0058fe0e6363b6c6c1b69
ochiengsulwe <kodongochieng@gmail.com>
2024-02-22 16:59:51 +0300

    Safer version of accepting input

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Takes the name to search as the last argument.
+
+    Displays all values where name matchesy7<F4> argument
+    """
+
+import MySQLdb
+import sys
+
+
+def arg_search(uname, passwd, dbname, nsearch):
+    """Searches for a name as parsed from the database.
+
+        Args:
+            uname (str): The username.
+            passwd (str): The user password.
+            dbname (str): The database we are querying.
+            nsearch (str): The state we are to search from the database.
+
+        Returns:
+            list: A list of the name occurances as they exists.
+    """
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             user=uname,
+                             passwd=passwd,
+                             db=dbname)
+
+        cur = db.cursor()
+
+        query = """SELECT * FROM `states` WHERE `name` LIKE BINARY '%s'
+        ORDER BY `id` ASC"""
+
+        cur.execute(query, (nsearch + "%",))
+
+        res = cur.fetchall()
+
+        for i in res:
+            print(i)
+
+    except MySQLdb.Error as e:
+        sys.exit(1)
+
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    uname, passwd = sys.argv[1], sys.argv[2]
+    dbname, nsearch = sys.argv[3], sys.argv[4]
+    arg_search(uname, passwd, dbname, nsearch)
