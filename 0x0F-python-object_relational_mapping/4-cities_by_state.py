89ec74b8cf52b10b5bc3bde2c6a5cd644195b366
ochiengsulwe <kodongochieng@gmail.com>
2024-02-22 19:24:01 +0300

    £

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Implements the `INNER JOIN` function in SQL."""
+
+import MySQLdb
+import sys
+
+
+def inner_join(uname, passwd, dbname):
+    """Joins two related tables on their association.
+
+        Args:
+            uname (str): The username.
+            passwd (str): The user password.
+            dbname (str): The database we are querying.
+
+        Returns:
+            list: A list of the name occurances as they exists.
+    """
+    try:
+        db = MySQLdb.connect(host='localhost',
+                             user=uname,
+                             passwd=passwd,
+                             db=dbname)
+
+        cur = db.cursor()
+
+        query = """
+        SELECT `cities.id`, `cities.name`, `states.name`
+        FROM `cities`
+        INNER JOIN `states` ON `cities.state_id` = `states.id`
+        ORDER BY `cities.id` ASC
+        """
+
+        cur.execute(query)
+
+        res = cur.fetchall()
+
+        for i in res:
+            print(i)
+
+    except MySQLdb.Error as e:
+        sys.exit(1)
+
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    uname, passwd = sys.argv[1], sys.argv[2]
+    dbname, nsearch = sys.argv[3], sys.argv[4]
+    arg_search(uname, passwd, dbname, nsearch)
