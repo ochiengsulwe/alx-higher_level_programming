f61b8ded8e5105f51dbc2e08159e71984c2d711b
ochiengsulwe <kodongochieng@gmail.com>
2024-02-23 19:50:30 +0300

    Binding sessions

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
