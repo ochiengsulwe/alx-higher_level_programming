61ae9fd9611535abe79a2d051989635993c16c9e
ochiengsulwe <kodongochieng@gmail.com>
2024-02-25 11:41:04 +0300

    Srcipt refactor to include method call

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,6 +5,30 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
+def print_states(username: str, password: str, database: str) -> None:
+    """Prints  all state objects from the states table.
+
+        Args:
+            username: The username of the db current user.
+            password: The user password to allow for connection to database.
+            database: The database we are querying.
+
+        Returns:
+            None: Prints queried results to the standard output.
+    """
+
+    conn_db = f"mysql://{username}:{password}@localhost:3306/{database}"
+    engine = create_engine(conn_db)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         sys.exit(1)
@@ -13,16 +37,8 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-    conn_db = f"mysql://{username}:{password}@localhost:3306/{database}"
-    engine = create_engine(conn_db)
+    print_states(username, password, database)
+
     Base.metadata.creat_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print(f"{state.id}: {state.name}")
 
     session.close()
